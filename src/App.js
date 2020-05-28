3f08ffe307c359fca3073665b42db1ad352123f7
Pavana Teja T <pavana@eligible.com>
2020-05-28 17:58:06 +0530

    Added Auth and API endpoints

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { API } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { coins: [], customers: [] };
+
+  async componentDidMount(){
+    try {
+      const data = await API.get('cryptoapi1', '/coins?limit=5&start=100')
+      console.log('data from Lambda REST API: ', data)
+      this.setState({ coins: data.coins })
+
+      const customersData = await API.get('customers', '/customers?limit=1')
+      console.log('data from Stripe customers REST API: ', customersData)
+      this.setState({ customers: customersData })
+    } catch (err) {
+      console.log('error fetching from Lambda API')
+      console.log('error: ' + err);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AmplifySignOut />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            this.state.coins.map((c, i) => (
+              <div key={i}>
+                <h2>{c.name}</h2>
+                <p>{c.price_usd}</p>
+              </div>
+            ))
+          }
+          {
+            this.state.customers.map((c, i) => (
+              <div key={i}>
+                <h2>{c.id}</h2>
+                <p>{c.object}</p>
+              </div>
+            ))
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
